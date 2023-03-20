363c1a1b372ba5a079eb45dceadcd70f072ccccb
iyhunko <iyhunko@gmail.com>
2023-03-20 22:56:12 +0200

    Add more tests for logger.go

--- pkg/logger/logger_test.go
+++ pkg/logger/logger_test.go
@@ -1,12 +1,30 @@
 package logger
 
 import (
+	"os"
+	"os/exec"
 	"testing"
 )
 
 var (
 	loggerErrorStr = "some error"
 )
+
+func TestLogger_ErrorWithExit(t *testing.T) {
+	lgr, _ := New()
+	if os.Getenv("CRASH") == "1" {
+		lgr.ErrorWithExit(loggerErrorStr)
+		return
+	}
+	cmd := exec.Command(os.Args[0], "test -test.run=TestLogger_ErrorWithExit")
+	cmd.Env = append(os.Environ(), "CRASH=1")
+	err := cmd.Run()
+
+	if e, ok := err.(*exec.ExitError); ok && !e.Success() {
+		return
+	}
+	t.Fatalf("process ran with err %v, expected exit status 1", err)
+}
 
 func TestLogger_Error(t *testing.T) {
 	lgr, _ := New()

586110b1bf53c2f0fb7af8a47ccf57c2a0e20a91
iyhunko <iyhunko@gmail.com>
2023-03-20 14:53:16 +0200

    Add config_test.go

---
+++ internal/config/config_test.go
@@ -0,0 +1,18 @@
+package config
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestConfig_NewConfig(t *testing.T) {
+	t.Run("default values", func(t *testing.T) {
+		conf := NewConfig(nil)
+		assert.Equal(t, "8001", conf.HTTPServerPort)
+		assert.Equal(t, "8002", conf.GRPCServerPort)
+		assert.Equal(t, "15s", conf.ReadTimeout.String())
+		assert.Equal(t, "15s", conf.WriteTimeout.String())
+		assert.Equal(t, "hash.json", conf.HashFilePath)
+		assert.Equal(t, "5s", conf.HashGenerationInterval.String())
+	})
+}

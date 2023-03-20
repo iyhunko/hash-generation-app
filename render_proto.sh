c207e0fbd0039abda8d123c99e28116736cf5ac1
iyhunko <iyhunko@gmail.com>
2023-03-20 16:50:20 +0200

    Remove render_proto.sh file

--- render_proto.sh
+++
@@ -1,5 +0,0 @@
-export PATH=$PATH:$HOME/go/bin
-export PATH=$PATH:/usr/local/go/bin
-protoc --go_out=. --go_opt=paths=source_relative \
---go-grpc_out=. --go-grpc_opt=paths=source_relative \
-proto/hash.go.proto

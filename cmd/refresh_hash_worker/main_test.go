f8873acfe43a30f062a7d6c8de54515254014d7e
Hunko Ivan <iyhunko@gmail.com>
2023-03-21 13:46:51 +0200

    Improve refresh_hash_worker code and add tests for it (#4)
    * Improve refresh_hash_worker code and add tests for it

---
+++ cmd/refresh_hash_worker/main_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/iyhunko/hash-generation-app/internal/config"
+	"github.com/iyhunko/hash-generation-app/internal/store"
+	"github.com/iyhunko/hash-generation-app/pkg/logger"
+	"github.com/stretchr/testify/assert"
+	"os"
+	"testing"
+)
+
+func TestMain_RefreshHash(t *testing.T) {
+	lgr, _ := logger.New()
+	conf := config.NewConfig(lgr)
+	storage := store.NewStore(lgr)
+	conf.HashFilePath = "test_hash.json"
+
+	t.Run("success_hash_refresh", func(t *testing.T) {
+		err := refreshHash(lgr, conf, storage)
+		assert.Empty(t, err)
+
+		_, err = os.OpenFile(conf.HashFilePath, os.O_RDWR, 0644)
+		assert.Nil(t, err)
+	})
+
+	t.Run("fail_empty_path", func(t *testing.T) {
+		conf.HashFilePath = ""
+
+		err := refreshHash(lgr, conf, storage)
+		assert.Equal(t, "open : no such file or directory", err.Error())
+	})
+}

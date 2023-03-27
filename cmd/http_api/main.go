4621b5db61bc4a994435e2a6f146d36abf4a640d
Hunko Ivan <iyhunko@gmail.com>
2023-03-27 22:57:48 +0300

    Add graceful shutdown for HTTP server (#6)
    * Add graceful shutdown for HTTP server

--- cmd/http_api/main.go
+++ cmd/http_api/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
 	"github.com/iyhunko/hash-generation-app/internal/config"
 	http2 "github.com/iyhunko/hash-generation-app/internal/http"
@@ -8,12 +10,15 @@ import (
 	"github.com/iyhunko/hash-generation-app/pkg/logger"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
 )
 
 const (
-	startingServerMsg  = "Starting http api server"
-	listeningToPortMsg = "Listening to %s port..."
-	listeningErrMsg    = "error listening %s"
+	startingServerMsg = "Starting http api server"
+	listeningErrMsg   = "error listening %w"
 )
 
 func main() {
@@ -35,6 +40,27 @@ func main() {
 		ReadTimeout:  conf.ReadTimeout,
 	}
 
-	lgr.Info(fmt.Sprintf(listeningToPortMsg, conf.HTTPServerPort))
-	lgr.FatalError(fmt.Errorf(listeningErrMsg, srv.ListenAndServe()))
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+	go startServer(srv, lgr)
+
+	<-done
+	lgr.Info("server stopped")
+	shutdown(context.Background(), srv, lgr)
+}
+
+func startServer(srv *http.Server, lgr logger.Logger) {
+	if err := srv.ListenAndServe(); err != nil && errors.Is(err, http.ErrServerClosed) {
+		lgr.FatalError(fmt.Errorf(listeningErrMsg, err))
+	}
+}
+
+func shutdown(ctx context.Context, server *http.Server, lgr logger.Logger) {
+	ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		lgr.FatalError(fmt.Errorf("server shutdown failed: %w", err))
+	}
+	lgr.Info("Server shutdown done")
 }
